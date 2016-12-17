9155d767b5d220a103479950cd5a234a88bee391
Bas Pennings <baspster@gmail.com>
2016-12-17 16:52:43 +0100

    Zapped most of the readme for now because it was old and misleading

--- src/nani_app.erl
+++ src/nani_app.erl
@@ -11,19 +11,3 @@ start(_StartType, StartArgs) ->
     end.
 
 stop(_State) -> ok.
-
-% -export([init/0, init/1]).
-
-% -define(DEFAULT_CONFIG, "./nani.config").
-
-% init() -> init(?DEFAULT_CONFIG).
-
-% init(ConfigFile) ->
-%     case file:consult(ConfigFile) of
-%         {ok, Config} ->
-%             nani_event:start(),
-%             nani_conn_info:add_handler(),  
-%             nani_bot:start(Config),
-%             nani_bot:connect();
-%         Err -> Err
-%     end.

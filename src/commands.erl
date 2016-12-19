7bb91e72fbde1d9517432e6aa6c8f6c8d06d506a
Bas Pennings <baspster@gmail.com>
2016-12-19 20:25:35 +0100

    Nice and flat now :)

--- src/commands.erl
+++ src/commands.erl
@@ -3,11 +3,13 @@
 -behavior(gen_event).
 
 %% API
--export([add_handler/0, delete_handler/0]).
+% -export([add_handler/0, delete_handler/0]).
 
 %% gen_event callbacks
--export([init/1, handle_event/2, handle_call/2, handle_info/2, 
-         terminate/2, code_change/3]).
+% -export([init/1, handle_event/2, handle_call/2, handle_info/2, 
+%          terminate/2, code_change/3]).
+
+-compile(export_all).
 
 %%%============================================================================
 %%% API
@@ -24,16 +26,23 @@ delete_handler() ->
 init([]) -> {ok, []}.
 
 handle_event({cmd, _Bot, From, To, Cmd}, State) ->
-    Str = binary_to_list(Cmd),
-    Tokens = lists:map(fun string:to_lower/1, string:tokens(Str, " ")),
+    Tokens = parse_cmd(Cmd),
+    Mod = commands,
     case Tokens of
         ["fac", Arg] -> 
-            handle_fac_command(From, To, Arg);
+            H = handle_fac_command,
+            MFA = {Mod, H, [Arg]},
+            apply_command(From, To, MFA);
         ["fmt", Fmt, Arg] ->
-            handle_fmt_command(From, To, Fmt, Arg);
+            H = handle_fmt_command,
+            MFA = {Mod, H, [Fmt, Arg]},
+            apply_command(From, To, MFA);
         ["fread", Fmt, Arg] ->
-            handle_fread_command(From, To, Fmt, Arg);
-        _ -> void
+            H = handle_fread_command,
+            MFA = {Mod, H, [Fmt, Arg]},
+            apply_command(From, To, MFA);
+        _ -> 
+            unknown_command(From, To, Cmd)
     end,
     {ok, State};
 
@@ -52,34 +61,34 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %%%============================================================================
 %%% Internal functions
 %%%============================================================================
-handle_fac_command(From, To, Arg) ->
-    case try_parse_int(Arg) of
-        {ok, Int} -> 
-            F = fac(Int),
-            nani_bot:say(To, [From, ": ", integer_to_list(F)]);
-        {error, _} -> 
-            nani_bot:say(To, [From, ": ", Arg, " is not a valid integer."])
+unknown_command(From, To, Cmd) ->
+    nani_bot:say(To, [From, ": ", "unknown command '", Cmd, "'"]).
+
+apply_command(From, To, {M, F, A}) ->
+    case apply(M, F, A) of 
+        {ok, Str} -> 
+            nani_bot:say(To, [From, ": ", Str]);
+        Err -> 
+            Msg = io_lib:format("~p", [Err]),
+            nani_bot:say(To, [From, ": ", Msg])
     end.
 
-handle_fmt_command(From, To, Fmt, Arg) ->
+handle_fac_command(Arg) ->
     case try_parse_int(Arg) of
-        {ok, Int} ->
-            case try_fmt_arg(Fmt, Int) of
-                {ok, Str} ->
-                    nani_bot:say(To, [From, ": ", Str]);
-                {error, _} ->
-                    nani_bot:say(To, [From, ": ", Fmt, " is not a valid format string."])
-            end;
-        {error, _} ->
-            nani_bot:say(To, [From, ": ", Arg, " is not a valid integer."])
+        {ok, Int} -> F = fac(Int), {ok, integer_to_list(F)};
+        Err -> Err
+    end. 
+
+handle_fmt_command(Fmt, Arg) ->
+    case try_parse_int(Arg) of
+        {ok, Int} -> try_fmt_arg(Fmt, Int);
+        Err -> Err
     end.
 
-handle_fread_command(From, To, Fmt, Arg) ->
+handle_fread_command(Fmt, Arg) ->
     case try_fread_arg(Fmt, Arg) of
-        {ok, Val} ->
-            nani_bot:say(To, [From, ": ", integer_to_list(Val)]);
-        {error, _} ->
-            nani_bot:say(To, [From, ": ", " either the format string or argument doesn't make sense."])
+        {ok, Val} -> {ok, integer_to_list(Val)};
+        Err -> Err
     end.
 
 try_fread_arg(Fmt, Arg) ->
@@ -104,3 +113,7 @@ try_parse_int(Str) ->
 
 fac(0) -> 1;
 fac(N) -> N * fac(N - 1).
+
+parse_cmd(Cmd) ->
+   Str = binary_to_list(Cmd),
+   lists:map(fun string:to_lower/1, string:tokens(Str, " ")).
